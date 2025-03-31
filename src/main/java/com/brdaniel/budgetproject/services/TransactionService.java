9281f8f0a98bad21ec8112066ba3673eb06fe02c
Bryan Daniel <>
2025-03-31 19:08:34 -0400

    Added insert transaction and get all transaction functionality to transaction service

--- src/main/java/com/brdaniel/budgetproject/services/TransactionService.java
+++ src/main/java/com/brdaniel/budgetproject/services/TransactionService.java
@@ -1,6 +1,25 @@
 package com.brdaniel.budgetproject.services;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.brdaniel.budgetproject.models.Transaction;
+
+// This class will handle all business logic for the Transactions List
 public class TransactionService {
-    // This class will handle all business logic for the Transactions List
+
+    // Initialize the DatabaseService
+    private DatabaseService databaseService = new DatabaseService();
+
+    // Method to add a transaction to the database
+    public void addTransaction(LocalDate localDate, String description, double amount, String category, String type) {
+        databaseService.insertTransaction(localDate, description, amount, category, type);
+    }
+
+    // Method to get all transactions from the database
+    public List<Transaction> getAllTransactions() {
+        return databaseService.getAllTransactions();
+    }
+
     
 }
