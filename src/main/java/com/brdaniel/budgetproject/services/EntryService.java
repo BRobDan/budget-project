c37de9bd30f48eee7a5cbce6ac22b45ca95ee17f
Bryan Daniel <>
2025-03-31 20:50:52 -0400

    Set up entry form and moved method for add transaction to proper service

--- src/main/java/com/brdaniel/budgetproject/services/EntryService.java
+++ src/main/java/com/brdaniel/budgetproject/services/EntryService.java
@@ -1,6 +1,13 @@
 package com.brdaniel.budgetproject.services;
 
+import java.time.LocalDate;
+
+// This class will handle all business logic for the Entry Form
 public class EntryService {
-    // This class will handle all business logic for the Entry Form
-    
+    private final DatabaseService databaseService = new DatabaseService();
+
+    // Method to add a transaction to the database
+    public void addTransaction(LocalDate localDate, String description, double amount, String category, String type) {
+        databaseService.insertTransaction(localDate, description, amount, category, type);
+    }
 }
--- src/main/java/com/brdaniel/budgetproject/services/TransactionService.java
+++ src/main/java/com/brdaniel/budgetproject/services/TransactionService.java
@@ -1,6 +1,5 @@
 package com.brdaniel.budgetproject.services;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import com.brdaniel.budgetproject.models.Transaction;
@@ -9,12 +8,7 @@ import com.brdaniel.budgetproject.models.Transaction;
 public class TransactionService {
 
     // Initialize the DatabaseService
-    private DatabaseService databaseService = new DatabaseService();
-
-    // Method to add a transaction to the database
-    public void addTransaction(LocalDate localDate, String description, double amount, String category, String type) {
-        databaseService.insertTransaction(localDate, description, amount, category, type);
-    }
+    private final DatabaseService databaseService = new DatabaseService();
 
     // Method to get all transactions from the database
     public List<Transaction> getAllTransactions() {
